38716710afc96df5827d1e3216bb59e60d04683e
Warwick Stone <uozu.aho@gmail.com>
2018-10-07 12:57:41 +1100

    WIP: py serial pixels implement displayer, use with demo

--- arduino_pixels/py_serial_pixels/main.py
+++ arduino_pixels/py_serial_pixels/main.py
@@ -15,6 +15,49 @@ def led_manager(serial_led):
     else:
         led.close()
 
+class SerialPixelDisplayer: # implements Displayer
+    def __init__(self, generator_func, port='/dev/ttyACM0', baud=57600):
+        self.generate = generator_func
+        self.port = port
+        self.baud = baud
+
+    def run(self):
+        with led_manager(SerialPixels(self.port, self.baud)) as strip:
+            while True:
+                pixels = self.generate()
+                strip.set(pixels)
+                time.sleep(0.02)
+
+class SerialPixels:
+    """ serial led strip interface, supporting Generator interface """
+    def __init__(self, port, baud, brightness_limit=255):
+        self.STOP_BYTE = 255
+        self.serial = serial.Serial(port, baud)
+        self.brightness_limit = brightness_limit
+
+    def set(self, pixels):
+        """ Set leds with pixel values
+            Parameters:
+                pixels: array of presenter.Pixel
+        """
+        rgb_array = [int(x) for pixel in pixels for x in pixel.rgb]
+        self._check_values(rgb_array)
+        self._set_leds(rgb_array)
+
+    def _check_values(self, rgb_array):
+        """ Ensure values are <= max brightness and != stop byte """
+        for i, value in enumerate(rgb_array):
+            if value > self.brightness_limit:
+                rgb_array[i] = self.brightness_limit
+            if value == self.STOP_BYTE:
+                rgb_array[i] -= 1
+
+    def _set_leds(self, rgb_array):
+        self.serial.write(bytes(list(rgb_array) + [self.STOP_BYTE]))
+
+    def close(self):
+        self.serial.close()
+
 class SerialLed:
     """ serial led strip interface """
     def __init__(self, port, baud, num_leds):
@@ -139,17 +182,17 @@ class Thing:
         for led in strip.leds:
             led.set_color(self.color)
 
-
-with led_manager(SerialLed('/dev/ttyACM0', 57600, NUM_LEDS)) as led:
-    strip = LedStrip(NUM_LEDS)
-    blob = LedStripBlob(NUM_LEDS)
-    blob.speed = 43
-    thing = Thing()
-    while True:
-        blob.update(time.time())
-        thing.update()
-        strip.clear()
-        thing.render(strip)
-        blob.render(strip)
-        led.set(strip)
-        time.sleep(0.02)
+if __name__ == '__main__':
+    with led_manager(SerialLed('/dev/ttyACM0', 57600, NUM_LEDS)) as led:
+        strip = LedStrip(NUM_LEDS)
+        blob = LedStripBlob(NUM_LEDS)
+        blob.speed = 43
+        thing = Thing()
+        while True:
+            blob.update(time.time())
+            thing.update()
+            strip.clear()
+            thing.render(strip)
+            blob.render(strip)
+            led.set(strip)
+            time.sleep(0.02)
--- demo.py
+++ demo.py
@@ -1,9 +1,20 @@
 from pixel_patterns.pixel_strip_patterns import BunchOfPatterns
-from pygame_pixels.pygame_pixels import create_displayer
 
-print('running neopixel pattern demo using pygame')
+# DISPLAYER = 'pygame'
+DISPLAYER = 'arduino'
+
+def create_displayer(generate_func):
+    if DISPLAYER == 'pygame':
+        print('using pygame to display pixels')
+        from pygame_pixels.pygame_pixels import create_displayer
+        raise 'uhh how to return a context managed thing?'
+    elif DISPLAYER == 'arduino':
+        print('using arduino to display pixels')
+        from arduino_pixels.py_serial_pixels.main import SerialPixelDisplayer
+        return SerialPixelDisplayer(generate_func)
+
 print('press any key to exit')
 
 patterns = BunchOfPatterns()
-with create_displayer(patterns.generate) as displayer:
-    displayer.run()
+displayer = create_displayer(patterns.generate)
+displayer.run()
