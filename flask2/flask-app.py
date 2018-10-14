bb868a27309f7b9166eae40edc1e476abdd7a9ee
Warwick Stone <uozu.aho@gmail.com>
2018-10-14 11:35:37 +1100

    cmd arg to set displayer

--- flask2/flask-app.py
+++ flask2/flask-app.py
@@ -6,34 +6,18 @@ from threading import Thread
 sys.path.append('..')
 
 from pixel_generator_slave import PixelGeneratorSlave
-from pygame_pixels.pygame_pixels import PygamePixelDisplayer
+
 
 DESCRIPTION = """
 Web server for remote control of neopixels.
 """
 
 NUM_PIXELS = 43
-DISPLAYER = 'pygame'
-# DISPLAYER = 'arduino'
 
 app = Flask(__name__, static_url_path='')
 app.config.from_object(__name__)
 pixelgen = PixelGeneratorSlave(NUM_PIXELS)
-if DISPLAYER == 'pygame':
-    pixeldisp = PygamePixelDisplayer(pixelgen.generate)
-elif DISPLAYER == 'arduino':
-    raise Exception('not implemented')
 
-# run the displayer in a background thread, since it runs a blocking loop
-dispthread = Thread(target=pixeldisp.run)
-dispthread.start()
-
-
-def parse_args():
-    arg_parser = argparse.ArgumentParser(description=DESCRIPTION)
-    arg_parser.add_argument('--host', help='set the host address of the server, ' +
-        'eg. 0.0.0.0 to accept external requests')
-    return arg_parser.parse_args()
 
 @app.route('/')
 def root():
@@ -59,8 +43,31 @@ def set_all():
 def set_all_rgb(r, g, b):
     pixelgen.set_all(r, g, b)
 
+def parse_args():
+    arg_parser = argparse.ArgumentParser(description=DESCRIPTION)
+    arg_parser.add_argument('--host', help='set the host address of the server, ' +
+        'eg. 0.0.0.0 to accept external requests')
+    arg_parser.add_argument('--device', help='set the display device (default: %(default)s)',
+        choices=['pygame', 'arduino'],
+        default='pygame')
+    return arg_parser.parse_args()
+
+def start_displayer(device):
+    if device == 'pygame':
+        from pygame_pixels.pygame_pixels import PygamePixelDisplayer
+        pixeldisp = PygamePixelDisplayer(pixelgen.generate)
+    elif device == 'arduino':
+        from arduino_pixels.py_serial_pixels.serial_pixel_displayer import SerialPixelDisplayer
+        pixeldisp = SerialPixelDisplayer(pixelgen.generate)
+    else:
+        raise Exception('unknown displayer device: ' + device)
+
+    # run the displayer in a background thread, since it runs a blocking loop
+    dispthread = Thread(target=pixeldisp.run)
+    dispthread.start()
 
 if __name__ == '__main__':
     args = parse_args()
     host = args.host if args.host else None
+    start_displayer(args.device)
     app.run(host)
