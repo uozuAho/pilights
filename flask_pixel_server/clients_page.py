7d7c040e07bb419cec21a876b185dbc92cb22215
Warwick Stone <uozu.aho@gmail.com>
2019-04-09 23:02:02 +1000

    clients page now sends and receives message from discovery server

--- flask_pixel_server/clients_page.py
+++ flask_pixel_server/clients_page.py
@@ -1,4 +1,21 @@
+import socket
+
 from flask import render_template
 
+
 def clients():
+    _send_clients_finder_broadcast()
     return render_template('clients.html', name='yoyoyo')
+
+
+def _send_clients_finder_broadcast():
+    skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    skt.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    # Set a timeout so the socket does not block
+    # indefinitely when trying to receive data.
+    skt.settimeout(0.2)
+    skt.bind(("", 37021))
+    message = b"just nod if you can hear me"
+    skt.sendto(message, ('<broadcast>', 37020))
+    data, addr = skt.recvfrom(1024)
+    print("received message: %s"%data)
