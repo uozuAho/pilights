6c72d700dcef5c48657c91b8d2f59d9951a2baec
Warwick Stone <uozu.aho@gmail.com>
2019-04-14 19:57:14 +1000

    print friend names on clients page

--- discovery_client/server.py
+++ discovery_client/server.py
@@ -2,11 +2,10 @@ import socket
 
 PORT_LISTEN = 37020
 PORT_SEND = 37021
+SERVER_NAME = 'joe'
 
 
 def main():
-    my_ip = get_my_ip()
-
     skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
     skt.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     skt.bind(("", PORT_LISTEN))
@@ -18,15 +17,7 @@ def main():
         print("received message: %s"%data)
         if data == "just nod if you can hear me":
             # todo: don't broadcast - send back to sender
-            skt.sendto(my_ip, ('<broadcast>', PORT_SEND))
-
-
-def get_my_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    my_ip = s.getsockname()[0]
-    s.close()
-    return my_ip
+            skt.sendto(SERVER_NAME, ('<broadcast>', PORT_SEND))
 
 
 if __name__ == "__main__":
--- flask_pixel_server/clients_page.py
+++ flask_pixel_server/clients_page.py
@@ -7,8 +7,8 @@ PORT_LISTEN_FOR_CLIENTS = 37021
 
 
 def clients():
-    _send_clients_finder_broadcast()
-    return render_template('clients.html', name='yoyoyo')
+    friend_addrs = _send_clients_finder_broadcast()
+    return render_template('clients.html', friends=friend_addrs)
 
 
 def _send_clients_finder_broadcast():
@@ -16,9 +16,9 @@ def _send_clients_finder_broadcast():
     skt.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     # Set a timeout so the socket does not block
     # indefinitely when trying to receive data.
-    skt.settimeout(0.2)
+    skt.settimeout(0.5)
     skt.bind(("", PORT_LISTEN_FOR_CLIENTS))
     message = b"just nod if you can hear me"
     skt.sendto(message, ('<broadcast>', PORT_BROADCAST_TO_CLIENTS))
     data, addr = skt.recvfrom(1024)
-    print("received message: %s"%data)
+    return [data, addr]
