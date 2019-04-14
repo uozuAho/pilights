8a5363fa4dbe1d94b4459eaa78e4a58f37958b63
Warwick Stone <uozu.aho@gmail.com>
2019-04-14 19:44:31 +1000

    discovery server now returns its ip

--- discovery_client/server.py
+++ discovery_client/server.py
@@ -3,15 +3,31 @@ import socket
 PORT_LISTEN = 37020
 PORT_SEND = 37021
 
-skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-skt.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-skt.bind(("", PORT_LISTEN))
 
-print('listening for messages on port ' + str(PORT_LISTEN))
+def main():
+    my_ip = get_my_ip()
 
-while True:
-    data, addr = skt.recvfrom(1024)
-    print("received message: %s"%data)
-    if data == "just nod if you can hear me":
-        # todo: don't broadcast - send back to sender
-        skt.sendto('nod', ('<broadcast>', PORT_SEND))
+    skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    skt.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    skt.bind(("", PORT_LISTEN))
+
+    print('listening for messages on port ' + str(PORT_LISTEN))
+
+    while True:
+        data, addr = skt.recvfrom(1024)
+        print("received message: %s"%data)
+        if data == "just nod if you can hear me":
+            # todo: don't broadcast - send back to sender
+            skt.sendto(my_ip, ('<broadcast>', PORT_SEND))
+
+
+def get_my_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    my_ip = s.getsockname()[0]
+    s.close()
+    return my_ip
+
+
+if __name__ == "__main__":
+    main()
