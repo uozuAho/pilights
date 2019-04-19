a1076a13020f6d14cc6005260dd523fc6a9843e2
Warwick Stone <uozu.aho@gmail.com>
2019-04-19 18:05:20 +1000

    add phat to flask app arg parser

--- flask_pixel_server/arg_parser.py
+++ flask_pixel_server/arg_parser.py
@@ -9,6 +9,6 @@ class ArgParser:
         arg_parser.add_argument('--host', help='set the host address of the server, ' +
             'eg. 0.0.0.0 to accept external requests')
         arg_parser.add_argument('--device', help='set the display device (default: %(default)s)',
-            choices=['pygame', 'arduino'],
+            choices=['pygame', 'arduino', 'phat'],
             default='pygame')
         return arg_parser.parse_args()
