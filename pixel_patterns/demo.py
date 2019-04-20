90ee5881b125cd8b33871c7d527cce26b4964e84
Warwick Stone <uozu.aho@gmail.com>
2019-04-20 11:47:05 +1000

    make demo more user friendly

--- pixel_patterns/demo.py
+++ pixel_patterns/demo.py
@@ -1,40 +1,53 @@
+import argparse
 import sys
+
 sys.path.append('..')
 from pixel_patterns.pixel_strip_patterns import BunchOfPatterns
 
-USAGE = 'usage: python3 demo.py [pygame|arduino|phat]'
+DESCRIPTION = 'display demo patterns on various light devices'
 
 
-DISPLAYER = 'pygame'
-if len(sys.argv) == 1:
-    DISPLAYER = 'pygame'
-elif len(sys.argv) == 2:
-    DISPLAYER = sys.argv[1]
-else:
-    print(USAGE)
+def main():
+    args = ArgParser(DESCRIPTION).parse_args()
+
+    def create_displayer(generate_func):
+        if args.device == 'pygame':
+            print('using pygame to display pixels')
+            from light_drivers.pygame_pixels.pygame_pixels import PygamePixelDisplayer
+            return PygamePixelDisplayer(generate_func)
+        elif args.device == 'arduino':
+            print('using arduino to display pixels')
+            from light_drivers.arduino_pixels.py_serial_pixels.serial_pixel_displayer import SerialPixelDisplayer
+            return SerialPixelDisplayer(generate_func)
+        elif args.device == 'phat':
+            print('using unicorn pHAT to display pixels')
+            from light_drivers.phat_pixels.phat_displayer import PhatDisplayer
+            return PhatDisplayer(generate_func)
+        else:
+            raise Exception('unknown displayer: ' + args.device)
+
+    print('press ctrl-c to exit')
+
+    patterns = BunchOfPatterns()
+    displayer = create_displayer(patterns.generate)
+    try:
+        displayer.run()
+    finally:
+        displayer.dispose()
 
 
-def create_displayer(generate_func):
-    if DISPLAYER == 'pygame':
-        print('using pygame to display pixels')
-        from light_drivers.pygame_pixels.pygame_pixels import PygamePixelDisplayer
-        return PygamePixelDisplayer(generate_func)
-    elif DISPLAYER == 'arduino':
-        print('using arduino to display pixels')
-        from light_drivers.arduino_pixels.py_serial_pixels.serial_pixel_displayer import SerialPixelDisplayer
-        return SerialPixelDisplayer(generate_func)
-    elif DISPLAYER == 'phat':
-        print('using unicorn pHAT to display pixels')
-        from light_drivers.phat_pixels.phat_displayer import PhatDisplayer
-        return PhatDisplayer(generate_func)
-    else:
-        raise Exception('unknown displayer: ' + DISPLAYER)
+class ArgParser:
+    def __init__(self, description):
+        self.description = description
 
-print('press any key to exit')
+    def parse_args(self):
+        arg_parser = argparse.ArgumentParser(description=self.description)
+        arg_parser.add_argument('--num_pixels', help='number of pixels on the device', type=int)
+        arg_parser.add_argument('--device', help='set the display device (default: %(default)s)',
+            choices=['pygame', 'arduino', 'phat'],
+            default='pygame')
+        return arg_parser.parse_args()
 
-patterns = BunchOfPatterns()
-displayer = create_displayer(patterns.generate)
-try:
-    displayer.run()
-finally:
-    displayer.dispose()
+
+if __name__ == "__main__":
+    main()
